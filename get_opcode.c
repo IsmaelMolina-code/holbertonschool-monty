546e7716606c55895008e3f46af75ff92f41c040
Mariana Echeto <echetomariana@gmail.com>
2023-05-16 17:22:38 -0300

    monty

--- get_opcode.c
+++ get_opcode.c
@@ -1,11 +1,37 @@
 #include "monty.h"
 
 /**
- * 
- * 
+ * get_op - valid code options
+ *
+ * @op: option to code
+ *
+ * @stack: double pointer
+ *
+ * @line_number:
+ *
+ * return: void
+ *
  */
-
-int get_opcode()
+void get_op (char *op, stack_t **stack, unsigned int line_number)
 {
-
+	size_t i;
+	instruction_t valid_cops[] ={
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		/*
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		*/
+	};
+	for (i = 0; valid_cops[i].opcode != NULL; i++)
+	{
+		if (strcmp(valid_cops[i].opcode, op) == 0)
+		{
+			valid_cops[i].f(stack, line_number);
+			return;
+		}
+	}
 }
